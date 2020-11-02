7eccea5ede04067dfbe40c0fc4955348a4d883b9
Black_J08 <nitin88mehta@gmail.com>
2020-11-03 00:07:12 +0530

    Added login route

--- binance_automation/urls.py
+++ binance_automation/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('index.urls')),
+    path('accounts/', include('user_auth.urls')),
 ]
