442e8f9a1c60fd0ed35e671024b87db42e732f21
Black_J08 <nitin88mehta@gmail.com>
2020-11-03 01:07:58 +0530

    Using django authentication to login users

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['login_user']
+        password = request.POST['login_password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            pass
+    else:
+        return render(request, 'login.html')
